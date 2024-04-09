4f9a60658af6838b823f856823806d95fa30c4f1
Kipchumba C. Bett <kbett68@gmail.com>
2024-04-09 16:07:08 +0300

    OZ-464: Add license header.

--- erpnext-openmrs/src/main/java/com/ozonehis/eip/erpnext/openmrs/routes/PatientRouting.java
+++ erpnext-openmrs/src/main/java/com/ozonehis/eip/erpnext/openmrs/routes/PatientRouting.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2024, Ozone HIS <info@ozone-his.com>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
 package com.ozonehis.eip.erpnext.openmrs.routes;
 
 import static com.ozonehis.eip.erpnext.openmrs.Constants.HEADER_ENABLE_PATIENT_SYNC;
